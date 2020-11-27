8c3bf6f6b30f8422c8d9f2dcdd630873e45afdf7
i2.ilin <i2.ilin@itransition.com>
2020-11-27 10:07:58 -0500

    rename Program.cs files for clarity

--- ChatServer/ServerProgram.cs
+++ ChatServer/ServerProgram.cs
@@ -6,12 +6,13 @@ namespace ChatServer
     class Program
     {
         const int Port = 50051;
+        const string MOTD = "Welcome to Hello Chatty server!";
 
         static void Main(string[] args)
         {
             Server server = new Server
             {
-                Services = { HelloChattyProtocol.HelloChatty.BindService(new HelloChatty()) },
+                Services = { HelloChattyProtocol.HelloChatty.BindService(new HelloChatty(MOTD)) },
                 Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
             };
             server.Start();
