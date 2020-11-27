5402c7d0e7533d67addd6b8ab44630505825fba9
i2.ilin <i2.ilin@itransition.com>
2020-11-27 08:31:59 -0500

    start implementing HelloChatty

---
+++ ChatServer/HelloChatty.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Core;
+using HelloChattyProtocol;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using static HelloChattyProtocol.HelloChatty;
+
+namespace ChatServer
+{
+    public class HelloChatty : HelloChattyBase
+    {
+        public override Task<NewMessageSendingResult> NewMessage(NewMessageContent message, ServerCallContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest clientInfo, ServerCallContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task SubscribeToMessages(RequestedChatInfo requestedChat, IServerStreamWriter<BroadcastedMessage> responseStream, ServerCallContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
