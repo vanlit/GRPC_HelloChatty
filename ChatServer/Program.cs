81b46fad2e5a6faf8c17e8a2965b2765b81b5104
i2.ilin <i2.ilin@itransition.com>
2020-11-27 08:52:59 -0500

    add basic server initialization

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using Grpc.Core;
 
 namespace ChatServer
 {
     class Program
     {
+        const int Port = 50051;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Server server = new Server
+            {
+                Services = { HelloChattyProtocol.HelloChatty.BindService(new HelloChatty()) },
+                Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine($"{nameof(HelloChatty)} server is listening on port {Port}");
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+
+            server.ShutdownAsync().Wait();
         }
     }
 }
